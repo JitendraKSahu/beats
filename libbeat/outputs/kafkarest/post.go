c88dfe3f92018571651e1068240149ed7e5447c9
Cloud User <centos@ip-172-31-5-38.us-west-2.compute.internal>
2020-08-25 10:19:55 +0000

    kafarest fix

--- libbeat/outputs/kafkarest/post.go
+++ libbeat/outputs/kafkarest/post.go
@@ -36,13 +36,12 @@ func sendToDest(url string, topic string, kafkaRecords []map[string]interface{})
 	//fmt.Println(records)
 
 	recordsData, err := json.Marshal(records)
-	fmt.Println(string(recordsData))
+	//fmt.Println(string(recordsData))
     if err != nil {
         fmt.Println(err)
         return err
     }
 
-	//fmt.Println("42  ....................")
 	fmt.Printf("No of records to be sent %d\n", len(kafkaRecords))
 	req, err := http.NewRequest("POST", kafkaUrl, bytes.NewBuffer(recordsData))
     if err != nil {
@@ -50,9 +49,8 @@ func sendToDest(url string, topic string, kafkaRecords []map[string]interface{})
         return  err
     }
 
-	//fmt.Println("......52  ....................")
 	req.Header.Set("Content-Type", "application/vnd.kafka.json.v2+json")
-	//req.Header.Set("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoiYWRtaW4vYWRtaW4iLCJpc3MiOiJsb2dhcmNoaXZhbCJ9.Aqhl-amaKaKDoXDc0-8TN4hhI7FFkLa76GwDMBTmR8s")
+	req.Header.Set("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoiYWRtaW4vYWRtaW4iLCJpc3MiOiJsb2dhcmNoaXZhbCJ9.Aqhl-amaKaKDoXDc0-8TN4hhI7FFkLa76GwDMBTmR8s")
 
     client := &http.Client{Timeout: 30 * time.Second}
 
